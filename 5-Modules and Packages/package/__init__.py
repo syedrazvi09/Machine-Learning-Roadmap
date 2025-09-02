b8cb42ae42be7774df6818bba279f6f7f4e6a59b
Syed Razvi <razvisyed2002@gmail.com>
2025-09-02 21:17:31 +0530

    Discussed Packages and Modules

---
+++ 5-Modules and Packages/package/__init__.py
@@ -0,0 +1,1 @@
+# __init__.py is a special file used in Python to define packages and intialize
---
+++ 5-Modules and Packages/test.py
@@ -0,0 +1,4 @@
+from package.math import *
+from package.subpackages.mult import *
+print(addition(4,5))
+print(multiply(3,4))
